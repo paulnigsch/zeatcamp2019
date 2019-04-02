fed3dcb34837b3d0c8719adc5e6fb666a7ddd4da
Paul Nigsch <p.nigsch@evva.com>
2019-04-02 11:44:00 +0200

    added timestamp processing

--- gpc-composer-files/fron_datacleaning.py
+++ gpc-composer-files/fron_datacleaning.py
@@ -3,6 +3,7 @@ code for cleaning sensor data - GCP version
 """
 
 from airflow import DAG
+from airflow.exceptions import AirflowException
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from airflow.contrib.operators.gcs_list_operator import GoogleCloudStorageListOperator
@@ -14,7 +15,7 @@ import logging
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018, 1, 29),
+    'start_date': datetime(2018, 1, 27),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -31,8 +32,8 @@ dag = DAG('fron-gpc-datacleaning', schedule_interval='@daily', default_args=defa
 ############################################################################
 
 def removeFile(path):
-    if os.path.isfile(dest):
-        os.remove(dest)
+    if os.path.isfile(path):
+        os.remove(path)
 
 def downloadGcSBlob(bucket, name, dest, remove_old=True):
     from google.cloud import storage
@@ -48,7 +49,21 @@ def downloadGcSBlob(bucket, name, dest, remove_old=True):
     blob.download_to_filename(dest)
 
     print("downloaded %s/%s to %s" %(bucket, blob, dest))
+    return True
 
+def uploadGcSBlob(bucket, source_file_name, destination_blob_name):
+
+    from google.cloud import storage
+    import os
+    storage_client = storage.Client()
+
+    bucket = storage_client.get_bucket(bucket)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+
+
+    print("file %s uploaded to %s" %(source_file_name, destination_blob_name))
+    return True
 
 def get_curent_file_path(ds, prefix=""):
     tmp = ds.replace("-", "_")
@@ -56,6 +71,39 @@ def get_curent_file_path(ds, prefix=""):
         prefix += '/'
     filepath = '%s%s/%s.csv' % (prefix, tmp[0:7], tmp)
     return filepath
+
+
+
+
+def get_in_out_paths(date, prev_stage, cur_stage):
+
+    if not isinstance(date, str):
+        raise AirflowException("wrong timeformat (should be a string)")
+
+
+    in_path = "%s/fronius" % prev_stage
+    out_path = "%s/fronius" % cur_stage
+
+    infile_path = get_curent_file_path(date, in_path)
+    outfile_path = get_curent_file_path(date, out_path)
+
+    return infile_path, outfile_path
+
+
+
+#ef ensure_folder_exits(folder):
+#   from google.cloud import storage
+#   storage_client = storage.Client()
+#   in_bucket = "landingarea"
+#   bucket = storage_client.get_bucket(in_bucket)
+#
+#   logging.info("trying to get %s", inpath)
+#
+#   ret = bucket.get_blob(inpath)
+#   if ret is None:
+#       logging.info("blob not found")
+#       import sys
+#       sys.exit(-1)
 
 
 
@@ -71,7 +119,7 @@ def input_is_present(ds, **context):
     path = "datalanding/fronius"
 
     logging.info("processing bucket: %s, path %s", in_bucket, path)
-    blob_path = get_curent_file_path(ds, '%s/%s' % (in_bucket, path))
+    blob_path = get_curent_file_path(ds, path)
 
     from google.cloud import storage
     storage_client = storage.Client()
@@ -80,15 +128,104 @@ def input_is_present(ds, **context):
     logging.info("trying to get %s", blob_path)
 
     ret = bucket.get_blob(blob_path)
-
     if ret is None:
         logging.info("blob not found")
+        import sys
+        sys.exit(-1)
         return False
+        raise AirflowException("blob not found")
 
     return True
 
 
+def add_timestamps(ds, **context):
+    import pandas as pd
 
+    inpath, outpath = get_in_out_paths(ds, prev_stage='datalanding', cur_stage='w_timestamps')
+
+    from google.cloud import storage
+    storage_client = storage.Client()
+    in_bucket = "landingarea"
+    bucket = storage_client.get_bucket(in_bucket)
+
+    logging.info("trying to get %s", inpath)
+
+    ret = bucket.get_blob(inpath)
+    if ret is None:
+        logging.info("blob not found")
+        import sys
+        sys.exit(-1)
+
+
+    ## copy data
+
+
+    from tempfile import mkstemp
+    fd, filename =  mkstemp()
+    if not downloadGcSBlob(in_bucket, inpath, filename, remove_old=True):
+        raise AirflowException("error while downloading blob")
+
+
+
+    data = pd.read_csv(filename, header=None)
+    column_names = ("Time", 'IP', 'Vibration', 'v2', 'Temperature')
+
+    data.columns =  column_names
+
+    data["Timestamp"] = data['Time'].apply( lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S").timestamp() )
+
+    invalid_temperature = data["Temperature"] < 0
+
+    data["Temperature"][invalid_temperature] = None
+
+    import os
+    fd, out_filename =  mkstemp(suffix=".msgpack")
+    data.to_msgpack(out_filename)
+
+    print(os.listdir())
+
+    logging.info("start uploading")
+
+    # todo: pani proper filehandling
+    uploadGcSBlob(in_bucket, out_filename, outpath[:-3] + 'msgpack')
+    return True
+
+
+def remove_duplicates(ds, **context):
+    import pandas as pd
+    import numpy as np
+    import os.path
+    import os
+
+    inpath, outpath = get_in_out_paths(ds, 'timestamps', cur_stage='duplicates_removed')
+    print('reading file from: %s' % (inpath + '.msgpack'))
+    d = pd.read_msgpack(inpath + '.msgpack')
+
+    def get_duplicates(data):
+        assert (data["Timestamp"].is_monotonic)
+        data_shifted = ((data["Timestamp"].shift(1) - data["Timestamp"]) == 0)
+        dups = data_shifted | data_shifted.shift(-1)
+        return dups
+
+    data_out = d.copy().set_index(["Timestamp", "IP"])
+    for grp, d in d.groupby("IP"):
+        print(grp)
+        duplicated_vals = d[get_duplicates(d)]
+        print('dups: ', len(duplicated_vals))
+        dd = duplicated_vals.groupby(by="Timestamp", sort=False, as_index=False)
+        ddd = dd.apply(lambda x: x.agg(
+            {'Time': lambda x: x.iloc[0], "IP": lambda x: x.iloc[0], "Vibration": np.mean, "v2": np.mean,
+             "Temperature": np.mean, "Timestamp": min}))
+        dddd = ddd.set_index(["Timestamp", "IP"]).sort_index()
+        data_out.loc[dddd.index] = dddd
+
+    data_out= data_out.reset_index()
+    no_dups = data_out.drop_duplicates()
+
+    os.makedirs(os.path.dirname(outpath), exist_ok=True )
+    no_dups.to_msgpack(outpath + '.msgpack')
+
+    return True
 
 
 ############################################################################
@@ -104,3 +241,13 @@ task_input_check = PythonOperator(
     #outlets={"datasets" :[outfile]},
     dag=dag)
 
+task_add_timestamps = PythonOperator(
+    task_id='add_timestamps',
+    provide_context=True,
+    python_callable=add_timestamps,
+    depends_on_past = True,
+    #inlets={"datasets" :[infile]},
+    #outlets={"datasets" :[outfile]},
+    dag=dag)
+
+task_input_check >> task_add_timestamps
